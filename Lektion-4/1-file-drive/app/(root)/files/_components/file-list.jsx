b0aeac644371c302c6a99bf87ad17f6a7c7db475
Joakim Lindh <joakim.lindh@epndata.com>
2024-04-25 14:06:15 +0200

    lektion 4

--- Lektion-4/1-file-drive/app/(root)/files/_components/file-list.jsx
+++ Lektion-4/1-file-drive/app/(root)/files/_components/file-list.jsx
@@ -6,13 +6,21 @@ import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import FileTable from "./file-table"
 import { Table, TableCaption } from "@/components/ui/table"
+import { Input } from "@/components/ui/input"
+import { Search, XIcon } from "lucide-react"
 
 const FileList = () => {
 
   const { user } = useAuth()
   const [files, setFiles] = useState(null)
+  const [search, setSearch] = useState('')
+  const [selectValue, setSelectValue] = useState('all')
 
-  // const normalizedFiles = files?.map()
+  const normalizedFiles = files?.map(file => ({
+    ...file,
+    createdAt: file.createdAt ? new Date(file?.createdAt?.seconds * 1000) : new Date()
+  }))
+  .filter(file => file.filename.toLowerCase().includes(search.toLocaleLowerCase()))
 
 
   useEffect(() => {
@@ -38,8 +46,14 @@ const FileList = () => {
 
   return (
     <div>
-      
-      <FileTable files={files} />
+      <div className="flex items-center justify-between mb-10">
+        <div className="relative w-1/2">
+          <Input value={search} onChange={e => setSearch(e.target.value)} className="px-12" />
+          <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 size-5 text-muted-foreground" />
+          <XIcon onClick={() => setSearch('')} className="absolute right-4 top-1/2 transform -translate-y-1/2 size-5 text-red-500/50 hover:text-red-500 cursor-pointer" />
+        </div>
+      </div>
+      <FileTable files={normalizedFiles} />
     </div>
   )
 }
--- Lektion-4/1-file-drive/app/(root)/files/_components/file-table.jsx
+++ Lektion-4/1-file-drive/app/(root)/files/_components/file-table.jsx
@@ -7,6 +7,12 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import HoverPreview from "./hover-preview"
+import { Button } from "@/components/ui/button"
+import { formatDistance } from "date-fns"
+import prettyBytes from "pretty-bytes"
+import { MoreVertical } from "lucide-react"
+import ActionsDropdown from "./actions-dropdown"
 
 
 const FileTable = ({ files }) => {
@@ -23,9 +29,33 @@ const FileTable = ({ files }) => {
       </TableHeader>
       <TableBody>
         {
-          files.length ? files.map(file => (
-            <TableRow>
-              <TableCell>{file.type}</TableCell>
+          !!files.length ? files.map(file => (
+            <TableRow key={file.id}>
+              <TableCell>{file.type.split('/')[1]}</TableCell>
+              <TableCell>
+                {
+                  file.type.includes('image')
+                  ? (
+                    <HoverPreview downloadURL={file.downloadURL}>
+                      <Button variant="ghost" size="sm">
+                        <a href={file.downloadURL} download target="_blank">
+                          {file.filename}
+                        </a>
+                      </Button>
+                    </HoverPreview>
+                  )
+                  :  (
+                    <Button variant="ghost" size="sm">
+                      <a href={file.downloadURL} download>
+                        {file.filename}
+                      </a>
+                    </Button>
+                  )
+                }
+              </TableCell>
+              <TableCell>{formatDistance(file.createdAt, new Date())}</TableCell>
+              <TableCell>{prettyBytes(file.size)}</TableCell>
+              <TableCell className="text-end"><ActionsDropdown downloadURL={file.downloadURL} id={file.id} filename={file.filename} /></TableCell>
             </TableRow>
           )): (
             <TableRow>
